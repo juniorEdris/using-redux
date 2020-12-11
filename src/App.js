fef54f5bc788584fd978f886ac48fab1b86d1513
eftekarRaghib <mdraghib17@gmail.com>
2020-12-12 00:39:24 +0600

    Redux hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import CakeStore from './Comp./CakeStore';
+import {Provider} from 'react-redux'
+import store from './redux/Store';
+import CakeHookCon from './Comp./CakeHookCon';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <CakeStore />
+        <CakeHookCon/>
+      </div>
+    </Provider>
   );
 }
 
